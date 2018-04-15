b2e6d5d1c7ca2340bff74d2a0eca812b1dc3512a
xxerror500xx <cliff.minks@gmail.com>
2018-04-15 10:13:52 -0500

    Setup basic project and add text input testing.

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,24 @@
+describe('Input form', () => {
+  beforeEach(() =>{
+    cy.visit('/')
+  })
+  it('focuses input on load', () => {
+    cy.focused()
+      .should('have.class', 'new-todo')
+  })
+
+  it('accepts input', () => {
+    const typedText = 'Buy Milk'
+    cy.get('.new-todo')
+      .type(typedText)
+      .should('have.value', typedText)
+  })
+
+  context('Form submission', () => {
+    it.only('Adds a new todo on submit', () => {
+      cy.get('.new-todo')
+        .type('Buy eggs')
+        .type('{enter}')
+    })
+  })
+})
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,6 +4,9 @@ export default props =>
   <form>
     <input
       type='text'
+      autoFocus
       className="new-todo"
+      value={props.currentTodo}
+      onChange={props.handleNewTodoChange}
       placeholder="What needs to be done?"/>
   </form>
